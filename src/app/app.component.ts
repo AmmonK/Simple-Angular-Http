f6cf95640dee351be88c28e2d7033b384f88985b
Ammon King <ammon.j.king@gmail.com>
2019-01-25 11:12:49 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+// import the json service
+import { JsonService } from "./json.service";
+// import the post model
+import { Post } from "./post";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'sample-angular';
+export class AppComponent implements OnInit {
+  // inject the json service
+  constructor(private jsonService: JsonService) {}
+
+  // data holder for the array of posts
+  posts: Post[];
+  // data holder for the post to be created
+  post: Post = new Post();
+
+  // method that subscribes to the service getPosts
+  // adds the response to the local posts array
+  getPosts(): void {
+    this.jsonService.getPosts().subscribe(posts => (this.posts = posts));
+  }
+
+  // metho that sends the post model to the service and gets a response
+  // adds to the end of the posts array
+  addPost(): void {
+    this.jsonService
+      .addPost(this.post)
+      .subscribe(post => (this.posts = [...this.posts, post]));
+    this.post = new Post();
+  }
+
+  ngOnInit(): void {
+    // get the list of posts when the component loads
+    this.getPosts();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+// add the module to use http methods
+import { HttpClientModule } from "@angular/common/http";
+// add the method to use [(ngModel)]
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
